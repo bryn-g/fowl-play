5a62e086b2393b66719cf35e6a5f3ac61307f795
bryn-g <22555149+bryn-g@users.noreply.github.com>
2022-08-28 17:40:00 +1000

    Added full R pathway with no intermediary files
    R process with reticulate package.

---
+++ get_blt_botscores_ret.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+uses the botometer-lite api endpoint to retrieve bot scores for twitter user ids
+
+- botometer api documentation states that botometer lite uses a lightweight model that allows detection of likely bots
+  in bulk.
+- unlike botometer-v4, lite just needs the user profile information and the timestamp of when the information was
+  collected to perform bot detection.
+- twitter mode:
+    -- the returned scores reflect the status of the accounts when the bot scores are requested, like with the
+       botometer-v4 endpoint
+    -- requires a rapid api and twitter api keys
+    -- uses botometer-lite check_accounts_from_user_ids function
+
+created on Mon Dec 20 18:01:28 2021
+
+@author: bryn-g
+"""
+
+import botometer
+import click
+import json
+import pandas as pd
+import sys
+import time
+from utils import *
+
+
+def get_blt_botscores(user_ids, wait_reset = True):
+    """ get bot scores for a list of twitter user ids using botometer-lite api endpoint in twitter mode """
+    # api keys
+    twitter_app_auth, rapid_api_key = get_api_keys()
+        
+    blt = botometer.BotometerLite(wait_on_ratelimit=wait_reset, **rapid_api_key, **twitter_app_auth)
+
+    # botometer lite api allows 200 requests per day with a max of 100 user ids per request
+    # therefore can get botscores for up to 20,000 accounts per day
+    res = ()
+    i = 1
+    for req_user_ids in list(chunks(user_ids, 100)):
+        dt_str = get_dt_str()
+
+        try:
+            blt_scores = blt.check_accounts_from_user_ids(req_user_ids)
+            res = (*res, pd.DataFrame(blt_scores, columns=["botscore", "user_id"]))
+        except Exception as err:
+            print(f"{err} - chunk {i}")
+        finally:
+            pass
+
+        i += 1
+        time.sleep(1)
+
+    # save scores to csv
+    df = pd.concat(res, ignore_index=True)
+    return df
---
+++ thread_botscores_ret.R
@@ -0,0 +1,77 @@
+# author: @bryn-g
+# date: 2022-08-26
+# ver: 1.0
+
+# collect a twitter conversation thread, use python scripts for bot scores
+# add bot scores into networks as node attributes
+
+# options
+options(scipen = 999)
+options(encoding = "UTF-8")
+
+library(dplyr)
+library(readr)
+library(reticulate)
+library(voson.tcn)
+
+# standard twitter api v2 bearer token
+token <- readRDS("~/.tcn_token")
+
+# use any tweet in conversation
+tweet_id <- "https://twitter.com/xxxxxx/status/xxxxxxxxxxxxxxxxx"
+
+# recent conversation thread
+thread <- tcn_threads(tweet_id, token = token, endpoint = "recent")
+
+# get conversation users
+user_ids <- as.list(thread$users |> distinct(user_id))
+
+# reticulate
+
+# if prefer to specify which miniconda - optional
+options(reticulate.conda_binary = "D:/PYTHON/miniconda3/condabin/conda.bat")
+
+if (!"r-fowl-play" %in% conda_list()$name) conda_create(envname = "r-fowl-play")
+use_condaenv(condaenv = "r-fowl-play")
+reqs <- readLines("requirements.txt")
+py_install(
+  reqs, envname = "r-fowl-play", conda = conda_binary(), pip = TRUE
+)
+
+# use more direct get_blt_botscores function
+reticulate::source_python("get_blt_botscores_ret.py")
+ret_bot_scores <- get_blt_botscores(
+  user_ids = r_to_py(user_ids)$user_id,
+  wait_reset = r_to_py(TRUE)
+)
+
+bot_scores <- ret_bot_scores |>
+  mutate_at(vars("user_id"), as.character) |>
+  # mutate_all(.f = as.character) |>
+  as_tibble() |> relocate("user_id") |>
+  rename_all(.funs = ~ paste0("bot.", .x)) |>
+  distinct(bot.user_id, .keep_all = TRUE)
+
+# create network
+net_actor <- thread |> tcn_network("actor")
+
+# add bot scores
+net_actor$nodes <- net_actor$nodes |>
+  left_join(bot_scores, by = c("user_id" = "bot.user_id"))
+
+  # plots
+require(igraph)
+require(visNetwork)
+
+pal <- colorRampPalette(c('#f1dde0','#bb0e6d'))
+net_actor$nodes$color <- pal(8)[cut(net_actor$nodes$bot.botscore, breaks = 8)]
+net_actor$nodes$label <- as.character(net_actor$nodes$bot.botscore)
+net_actor$nodes <- net_actor$nodes |>
+  mutate(color = ifelse(is.na(color), "#f5f5f5", color))
+
+# actor graph
+g_actor <- graph_from_data_frame(
+  net_actor$edges, vertices = net_actor$nodes
+) |> visIgraph(idToLabel = FALSE)
+
+g_actor
